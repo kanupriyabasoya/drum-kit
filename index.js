9896c3a28df15f1a2b82e143813b11414aa36f01
kanupriyabasoya <kanupriyakenny@gmail.com>
2022-06-23 11:43:16 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ function playSound(e){
 const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 const key = document.querySelector(`section[data-key="${e.keyCode}"]`);
 
-if(!audio) return;
+if(!audio) return;//stop the function running altogether
 
 key.classList.add('playing');//aading playing to increase size of content
-audio.currentTime = 0;//to keep playing the audio the no. of times we press key
+audio.currentTime = 0;//to keep playing the audio the no. of times we press key(rewind to the start)
 audio.play();//to play audio
 
 }
+function transformFunc(e){
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
 
+const keys= document.querySelectorAll('.key');
+keys.forEach(element => {
+    element.addEventListener('transitionend',transformFunc);
+});
 window.addEventListener('keydown',playSound);
+
 
 
